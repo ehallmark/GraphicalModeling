7c20e9d987709a4dde1429981e0dacbcfa48f566
Evan Hallmark <ehallmark@gttgrp.com>
2017-04-13 13:44:11 -0700

    src

---
+++ src/main/java/graph/Edge.java
@@ -0,0 +1,7 @@
+package graph;
+
+/**
+ * Created by ehallmark on 4/13/17.
+ */
+public class Edge {
+}
---
+++ src/main/java/graph/Node.java
@@ -0,0 +1,11 @@
+package graph;
+
+import java.util.List;
+
+/**
+ * Created by ehallmark on 4/13/17.
+ */
+public class Node {
+    private List<Edge> neihborhood;
+    private String label;
+}
