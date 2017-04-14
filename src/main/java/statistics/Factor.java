06cd45ce042647901371b49d74d798f0f326e0a3
Evan Hallmark <ehallmar@usc.edu>
2017-04-14 07:31:54 -0700

    changes

--- src/main/java/statistics/Factor.java
+++ src/main/java/statistics/Factor.java
@@ -1,8 +1,18 @@
 package statistics;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+
 /**
  * Created by Evan on 4/13/2017.
  */
 public class Factor {
-    
+    private INDArray weights;
+
+    public Factor(INDArray weights) {
+        this.weights=weights;
+    }
+
+    public Factor multiply(Factor other) {
+        weights.
+    }
 }
