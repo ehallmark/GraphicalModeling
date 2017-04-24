1bb19070ff94bd92a6081dcc2dcd677af3074f61
Evan Hallmark <ehallmar@usc.edu>
2017-04-24 03:59:00 -0700

    changes

--- src/main/java/model/graphs/MarkovNet.java
+++ src/main/java/model/graphs/MarkovNet.java
@@ -1,5 +1,9 @@
 package model.graphs;
 
+import model.nodes.Node;
+
+import java.util.List;
+
 /**
  * Created by Evan on 4/24/2017.
  */
@@ -7,5 +11,15 @@ public class MarkovNet extends Graph {
     // undirected graph
     public MarkovNet() {
         super(false);
+    }
+
+    // Returns triangulated (chordal) version of this graph
+    public MarkovNet triangulated() {
+        return null;
+    }
+
+    // Make sure the graph is triangulated, or one may not exist!
+    List<Node> findPerfectEliminitationOrdering() {
+        return null;
     }
 }
