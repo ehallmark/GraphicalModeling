251edd95a415e9fe1564bda450907f36b010c42c
Evan Hallmark <ehallmark@gttgrp.com>
2017-04-26 14:45:18 -0700

    mini example

--- src/main/java/examples/BeliefPropagationExample.java
+++ src/main/java/examples/BeliefPropagationExample.java
@@ -73,7 +73,7 @@ public class BeliefPropagationExample {
              Scope: [Node 1, Node 2]
              Factor: [0.1, 0.2, 0.3, 0.4]
          **/
-        
+
         // Run Belief Propagation
 
     }
